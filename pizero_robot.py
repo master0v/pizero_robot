51c6ae2f9fbd8ce1107cd3b919ed836991df312d
masterov <alexei@masterov.us>
2021-09-20 12:18:32 -0400

    10 attempts to get ip

--- pizero_robot.py
+++ pizero_robot.py
@@ -19,24 +19,32 @@ logger.debug("Turning on LED")
 lc=ledControl()
 lc.redColorWipe()
 
-sleep(3)
-
 # get ip address
-logger.debug("Getting the IP using 'ifconfig'")
-wlan0 = str(check_output(["ifconfig", "wlan0"]))
-matches = re.findall(r'inet \d+[.]\d+[.]\d+[.]\d+', wlan0) 
-if matches:
-  ip = matches[0].split()[1]
-else:
-  ip = "Can't get ip"
+attempt = 0
+while True:
+  logger.debug("Getting the IP using 'ifconfig' (attempt {attempt})")
+  wlan0 = str(check_output(["ifconfig", "wlan0"]))
+  matches = re.findall(r'inet \d+[.]\d+[.]\d+[.]\d+', wlan0) 
+  if matches:
+    ip = matches[0].split()[1]
+    logger.debug(f" got {ip}")
+    break
+  elif attempt > 10:
+    ip = "Can't get ip"
+    break
+  attempt+=1
+  
+lc.blueColorWipe()
 
 # send it to telegram
+logger.debug("sending '{ip}' to telegram (attempt {attempt})")
 bot = telegram.Bot(token=telegram_token.BOT_TOKEN)
 try:
-  logger.debug(f"sending '{ip}' to telegram")
   bot.send_message(chat_id=telegram_token.CHAT_ID, text=f"RaspiTank got IP {ip}")
 except Exception as e:
   logger.warning(f"Can't send to telegram: {e}")
+
+lc.redColorWipe()
 
 # Termination signals callback
 def exit_gracefully(signum, frame):
@@ -50,4 +58,4 @@ signal.signal(signal.SIGTERM, exit_gracefully)
 
 while True:
   logger.info(".")
-  sleep(3)
+  sleep(10)
